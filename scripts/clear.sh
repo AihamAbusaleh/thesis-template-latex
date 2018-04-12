fbd22856e26f8bceab19145f16cf0bb9fb657d60
Aiham Abusaleh <aiham.abusaleh@yahoo.com>
2018-04-12 22:31:59 +0200

    updated

--- scripts/clear.sh
+++ scripts/clear.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-files="*.log *.aux *.lot *.lol *.lof *.synctex.gz *.toc *.bbl *.blg *.dvi *.ilg *.nlo *.nls *.out"
+files="*.log *.aux *.lot *.lol *.lof *.synctex.gz *.toc *.bbl *.blg *.dvi *.ilg *.nlo *.nls *.out *-blx.bib *.xml *.bcf"
 cd ../../  # go to the BA Folder and search the files in the folders inside BA with "find ."
 for f in $files; do  find . -name "$f" -print0 | xargs -0 rm -f; done
 
